cb2669a9693b224a301c35ef6b12f636e8df983f
Ali Almohaya <AliAlmohaya@gmail.com>
2018-05-13 17:43:46 +0300

    add some to sw

--- public/sw.js
+++ public/sw.js
@@ -1,2 +1,17 @@
-// this is my really first SW
+// this is my really first SW XD
+self.addEventListener('install', event => {
+    console.log('[Service Worker] => installing SW :', event);
+});
 
+self.addEventListener('activate', event => {
+    console.log('[Service Worker] => activating SW :', event);
+    self.clients.claim();
+});
+
+self.addEventListener('fetch', event => {
+    console.log('[Service Worker] => fetching :', event);
+});
+
+self.addEventListener('push', event => {
+    console.log('[Service Worker] => pushing :', event);
+})
