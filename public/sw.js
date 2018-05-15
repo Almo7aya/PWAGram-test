5e512cb28be0bd8289e718f23997f402ac519ce7
Ali Almohaya <AliAlmohaya@gmail.com>
2018-05-15 20:06:04 +0300

    cache some asserts

--- public/sw.js
+++ public/sw.js
@@ -3,7 +3,23 @@ self.addEventListener('install', function(event) {
   console.log('[Service Worker] Installing Service Worker ...', event);
   event.waitUntil(
     caches.open('static').then(cache => {
-      cache.add('/src/js/app.js');
+      console.log('[Service Worker] caching ...');
+      cache.addAll([
+        '/',
+        'index.html',
+        '/src/js/app.js',
+        '/src/js/material.min.js',
+        '/src/js/promise.js',
+        '/src/js/fetch.js',
+        '/src/js/feed.js',
+        'https://fonts.googleapis.com/css?family=Roboto:400,700',
+        'https://fonts.googleapis.com/icon?family=Material+Icons',
+        'https://cdnjs.cloudflare.com/ajax/libs/material-design-lite/1.3.0/material.indigo-pink.min.css',
+        '/src/css/app.css',
+        '/src/css/feed.css',
+        '/manifest.json',
+        ''
+      ]);
     })
   )
 });
@@ -14,5 +30,11 @@ self.addEventListener('activate', function(event) {
 });
 
 self.addEventListener('fetch', function(event) {
-  event.respondWith(fetch(event.request));
+  event.respondWith(
+    caches.match(event.request)
+    .then(response => {
+      if (response) return response;
+      return fetch(event.request);
+    })
+);
 });
